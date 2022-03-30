fc4677ce9ef32b66cb3d6eb11537c761b7134abf
Raznan <muhammadraznan@satpaminternet.com>
2022-03-30 23:47:53 +0700

    Base

---
+++ Scenes/ObjectNaming/TimerBar.gd
@@ -0,0 +1,30 @@
+extends ProgressBar
+
+signal timeout
+
+
+onready var timer: Timer = $Timer
+
+
+func _ready() -> void:
+	start()
+
+
+func start() -> void:
+	set_process(true)
+	
+	timer.start(max_value)
+
+
+func end() -> void:
+	set_process(false)
+	emit_signal("timeout")
+
+
+func _process(delta: float) -> void:
+#	if timer.time_left
+	value = timer.time_left
+
+
+func _on_Timer_timeout() -> void:
+	end()
