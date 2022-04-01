c8be622b3c9f95b4f5c44f113e91631b55f74ce3
Raznan <muhammadraznan@satpaminternet.com>
2022-04-01 15:18:42 +0700

    Randomize object with rng bag algorithm

--- Scenes/ObjectNaming/ObjectNaming.gd
+++ Scenes/ObjectNaming/ObjectNaming.gd
@@ -1,28 +1,68 @@
 extends Control
 
+const NAME_LIST := [
+	"cow",
+	"dog",
+	"horse",
+	"chicken",
+	"duck",
+	"sheep",
+	"cat",
+	"rabbit",
+]
+
+# Equal RNG Bagging algorithm
+class RNGBagSlot:
+	var item_bag : Array
+	var _unique_items := []
+	
+	func _init(item_list : Array) -> void:
+		item_bag = []
+		_unique_items = item_list
+
+#########    THIS SECTION IS MODIFIABLE
+		_add_equal(item_list)
+	
+	func _add_equal(item_list : Array) -> void:
+		for item in item_list:
+		   #########   THIS IS OPTIONAL
+			# The amount (3) can be change to N for either less/more rebalancing.
+			# This controls the depth.
+			for _i in range(3):
+				item_bag.push_back(item)
+
+	func get_random_item():
+		var selected_item = item_bag[randi() % item_bag.size()]
+		
+		item_bag.erase(selected_item)
+
+		if item_bag.count(selected_item) <= 1:
+			 # Bag rebalancing
+			item_bag.append_array(_unique_items)
+			
+		return selected_item
+
 
 var object_name: String
+var name_bag: RNGBagSlot
+
+
+onready var object_rect: TextureRect = $ObjectRect
 
 
 func _ready() -> void:
+	name_bag = RNGBagSlot.new(NAME_LIST)
+	
 	randomize()
-	randomize_object_name()
-	
-	print_debug(object_name)
+	randomize_object()
 
 
-func randomize_object_name() -> void:
-	var NAME_LIST := [
-		"cow",
-		"dog",
-		"horse",
-		"chicken",
-		"duck",
-		"sheep",
-		"cat",
-		"rabbit",
-	]
+func randomize_object() -> void:
+	var TEX_DIR := "res://Resources/Animals/"
 	
-	var idx := randi() % (NAME_LIST.size() - 1)
+	object_name = name_bag.get_random_item()
 	
-	object_name = NAME_LIST[idx]
+	var res_path := TEX_DIR + object_name + ".png"
+	var texture: Texture = load(res_path)
+	
+	object_rect.texture = texture
