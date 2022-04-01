40a7fd7428317f6421dbe698680e39264f427a09
Raznan <muhammadraznan@satpaminternet.com>
2022-04-02 04:36:42 +0700

    set spanner accordingly on flag reach

--- Scenes/ObjectNaming/OverDialog.gd
+++ Scenes/ObjectNaming/OverDialog.gd
@@ -7,5 +7,23 @@ onready var point_gain: Control = $VBox/PointGain
 onready var spanner: Control = $VBox/Spanner
 
 
+func _ready() -> void:
+	connect("visibility_changed", self, "_on_visibility_changed")
+
+
+func _on_visibility_changed() -> void:
+	if not visible:
+		return
+	
+	var points := RuntimeManager.point_accumulated
+	
+	point_gain.point = points
+	
+	if points >= 0:
+		spanner.show_gain()
+	else:
+		spanner.show_loss()
+
+
 func _on_Back_pressed() -> void:
 	emit_signal("back_to_menu_request")
