75ed42555e766637067a2379b5dc824d992f80bb
Raznan <muhammadraznan@satpaminternet.com>
2022-04-17 14:09:07 +0700

    Handle profile navigation

--- Scenes/TitleScreen/ProfileDialog.gd
+++ Scenes/TitleScreen/ProfileDialog.gd
@@ -1,16 +1,11 @@
 extends Panel
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+func _on_Back_pressed() -> void:
+	hide()
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _on_Logout_pressed() -> void:
+	RuntimeManager.login = false
+	
+	hide()
--- Scenes/TitleScreen/TitleScreen.gd
+++ Scenes/TitleScreen/TitleScreen.gd
@@ -2,6 +2,7 @@ extends Control
 
 onready var screens: Control = $Screens
 onready var bottom_button: Control = $BottomButton
+onready var profile_dialog: Control = $ProfileDialog
 
 
 func _ready() -> void:
@@ -10,7 +11,7 @@ func _ready() -> void:
 	
 	if RuntimeManager.login:
 		screens.show_screen(screens.user_screen)
-		bottom_button.show_button(bottom_button.logout)
+		bottom_button.show_button(bottom_button.profile)
 
 
 func _on_Guest_pressed() -> void:
@@ -23,6 +24,7 @@ func _on_Guest_pressed() -> void:
 func _on_Logout_pressed() -> void:
 	screens.show_screen(screens.login_screen)
 	bottom_button.show_button(bottom_button.create_account)
-	
-	RuntimeManager.login = false
 
+
+func _on_Profile_pressed() -> void:
+	profile_dialog.show()
