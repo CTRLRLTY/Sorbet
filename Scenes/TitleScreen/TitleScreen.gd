e8eb2dee03548b251b4873a1d248ae655bba39a8
Scr1pti3 <52656050+scr1pti3@users.noreply.github.com>
2022-06-21 19:49:03 +0700

    Setup Prototype Export
    Set name for placeholder and ignore nakama calls

--- Scenes/TitleScreen/TitleScreen.gd
+++ Scenes/TitleScreen/TitleScreen.gd
@@ -18,7 +18,9 @@ func _on_Guest_pressed() -> void:
 	screens.show_screen(screens.user_screen)
 	bottom_button.show_button(bottom_button.profile)
 	
-	Server.login_device()
+	RuntimeManager.login = true
+	
+#	Server.login_device()
 	
 #	OS.shell_open("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile&client_id=990459061327-el1bs4q0tdk7e3odd301md3mdatru0sk.apps.googleusercontent.com&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code")
 
@@ -26,8 +28,8 @@ func _on_Guest_pressed() -> void:
 func _on_Logout_pressed() -> void:
 	screens.show_screen(screens.login_screen)
 	bottom_button.show_button(bottom_button.create_account)
-	
-	Server.logout()
+	RuntimeManager.login = false
+#	Server.logout()
 
 
 func _on_Profile_pressed() -> void:
